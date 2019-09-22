83952b52dda61ccef9f9cd257d8f21af75ccea5d
dhairya dholu <dhairyadholu@gmail.com>
2019-09-22 16:08:45 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const a=10;
+const b=10;
+const c=10;
+
+if(a>b && a>c)
+{
+    console.log('a is maximum');
+}
+else if (b>a && b>c)
+{
+    console.log('b is max');
+}
+else if (a==b && b==c)
+{
+    console.log('number are same');
+}
+else 
+{
+    console.log('c is max');
+}
